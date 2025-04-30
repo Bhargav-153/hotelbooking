f4308971840b6def76532a9c9e3cbaf3e91f61ba
Bhargav sirja <bhargav.sirja162@gmail.com>
2025-04-30 16:32:52 +0530

    index and show route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
 
 const MONGO_URL = "mongodb://localhost:27017/wanderlust";
 
@@ -17,22 +18,39 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hey i am root");
 });
 
-app.get("/testListing", async (req, res) => {
-  let sampleListing = new Listing({
-    title: "My new villa",
-    description: "by the beach",
-    price: 1200,
-    location: "Calangute, goa",
-    Country: "India",
-  });
-  await sampleListing.save();
-  console.log("sample was saved to db");
-  res.send("successful testing");
+// index route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 });
+
+// show route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My new villa",
+//     description: "by the beach",
+//     price: 1200,
+//     location: "Calangute, goa",
+//     country: "India",
+//   });
+//   await sampleListing.save();
+//   console.log("sample was saved to db");
+//   res.send("successful testing");
+// });
 
 app.listen(8080, () => {
   console.log("server is listening to port 8080");
---
+++ init/data.js
@@ -0,0 +1,66 @@
+const sampleListings = [
+  {
+    title: "My new villa",
+    description: "By the beach",
+    image:
+      "https://plus.unsplash.com/premium_photo-1677093726295-62da6b372694?q=80&w=1991&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: 1200,
+    location: "Calangute, Goa",
+    country: "India",
+  },
+  {
+    title: "Luxury Penthouse",
+    description: "City skyline view",
+    image:
+      "https://plus.unsplash.com/premium_photo-1673139100672-eb9ed3a7b8f9?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: 8500,
+    location: "Bandra, Mumbai",
+    country: "India",
+  },
+  {
+    title: "Countryside Cottage",
+    description: "Peaceful retreat",
+    image:
+      "https://plus.unsplash.com/premium_photo-1673138808212-f44935ec2c96?q=80&w=1940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: 3200,
+    location: "Coonoor, Tamil Nadu",
+    country: "India",
+  },
+  {
+    title: "Modern Studio",
+    description: "Ideal for singles",
+    image:
+      "https://plus.unsplash.com/premium_photo-1673138808212-f44935ec2c96?q=80&w=1940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: 1800,
+    location: "Koramangala, Bengaluru",
+    country: "India",
+  },
+  {
+    title: "Family Bungalow",
+    description: "Spacious and green",
+    image:
+      "https://plus.unsplash.com/premium_photo-1677093727026-3f3135db449c?q=80&w=1946&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: 4600,
+    location: "Nagpur, Maharashtra",
+    country: "India",
+  },
+  {
+    title: "Himalayan Retreat",
+    description: "Mountain escape",
+    image:
+      "https://plus.unsplash.com/premium_photo-1676587151813-19beea409ada?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: 2800,
+    location: "Manali, Himachal Pradesh",
+    country: "India",
+  },
+  {
+    title: "Beachside Apartment",
+    description: "Walk to the shore",
+    image:
+      "https://plus.unsplash.com/premium_photo-1676587160050-08c4c2b0972c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: 3900,
+    location: "Varkala, Kerala",
+    country: "India",
+  },
+];
+module.exports = { data: sampleListings };
